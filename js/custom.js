8a17f82ecbe0225b8900aba5d1e6a5a11f33708d
panamparan <84341321+panamparan@users.noreply.github.com>
2021-07-21 17:20:45 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+/*
+#TheSparksFoundationInternship
+#GripJuly21
+#WebDevInternship
+#Task3
+#PaymentGatewayIntegration	
+#panamparanarasu
+*/
+
+$(document).ready(function() {
+	$('#HomePage').css({height:$(window).height(),width:$(window).width()});
+	$(window).resize(function(){
+        $('#HomePage').css({height:$(window).height(),width:$(window).width()});
+    });
+
+});
+
